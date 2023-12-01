357be726e7859db2ab69469d62ee925e116be2f5
oxtrades69 <oxtrades69@gmail.com>
2023-12-01 08:26:06 +0530

    commited

---
+++ dev.java
@@ -0,0 +1,7 @@
+hi friends this is practice sseion for git and git hub
+
+
+hi friends this is suarshan chokkam iam working as devops engineer at tcs hyd
+this is a file we can create for java web applcation
+iam storing this file in devs branch
+
